118455a8d5325fd13e9adba4b7b5ead14ed1ee7b
PabloGomez03 <jbarea155@gmail.com>
2025-10-16 13:23:40 +0200

    Cambios en el modelo

--- src/java/Models/Product.java
+++ src/java/Models/Product.java
@@ -13,7 +13,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.Constraint;
 import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 
